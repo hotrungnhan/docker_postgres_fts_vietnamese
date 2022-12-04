e3bd8713db4533b9896ac6db6a72c2ab1ce10e78
Hồ Trung Nhân <hotrungnhan29@gmail.com>
2022-12-04 21:50:33 +0700

    update image name scheme

--- upgrade.py
+++ upgrade.py
@@ -14,8 +14,8 @@ for [key,value] in read_json('./version.json').items():
     # platform param
     platform = f"--platform {','.join(value['platform'])}"
     # tag param
-    tags = value["tag"] + [f"{key}_{FTS_VERSION}"]
-    tags= map(lambda t:f"-t {IMAGE_REPOSITORY}:{t}",value["tag"])
+    tags = value["tag"] + [f"{key}-{FTS_VERSION}"]
+    tags= map(lambda t:f"-t {IMAGE_REPOSITORY}:{t}",tags)
     tags= " ".join(tags)
 
     print(f"docker buildx build --build-arg POSTGRES_VERSION={key} --build-arg FTS_VERSION={FTS_VERSION} {platform} {tags} --push .")
